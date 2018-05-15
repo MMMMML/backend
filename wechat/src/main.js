1bd8d1e99c62a267cc198174f4ae59808072d2d2
丁德星 <dexing.ding@flashelp.cn>
2018-05-15 11:25:43 +0800

    commit

--- wechat/src/main.js
+++ wechat/src/main.js
@@ -53,10 +53,10 @@ axios.interceptors.response.use(
       const redirect = `http://aj.kingwingaviation.com/alliance-java/wechat/auth/fuwuLogin?state=${encodeURIComponent(location.href)}`
       window.location.href = redirect
     } else if (error.toString().includes('400')) {
-      let url = location.host + '/#/attestation?redirect=mine'
+      let url = location.host + location.pathname +  '#/attestation?redirect=mine'
       setTimeout(() => {
         window.location.href = '/#/attestation?redirect=mine'        
-      }, 800)
+      }, 500)
     }
     return Promise.reject(error)
   }
