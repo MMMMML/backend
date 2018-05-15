e59a1e874e18fc4f1b4aa8569cccebbe597ac4a5
丁德星 <dexing.ding@flashelp.cn>
2018-05-15 10:18:34 +0800

    add some bug~

--- wechat/src/main.js
+++ wechat/src/main.js
@@ -13,21 +13,21 @@ import axios from 'axios'
 import router from './router'
 import { getSessionId, getWxFrom } from '@/util'
 
-// import VConsole from 'vconsole'
+import VConsole from 'vconsole'
 import { Field, DatetimePicker, Radio, Indicator } from 'mint-ui'
 Vue.component(Field.name, Field)
 Vue.component(DatetimePicker.name, DatetimePicker)
 
-// let vConsole = new VConsole()
+let vConsole = new VConsole()
 
 FastClick.attach(document.body)
 
 // 正式
 // axios.defaults.baseURL = 'http://aj.kingwingaviation.com/alliance/api/'
 // 调试
-// axios.defaults.baseURL = '/apis'
+axios.defaults.baseURL = '/apis'
 // 测试
-axios.defaults.baseURL = 'http://aj.kingwingaviation.com/alliance-java/'
+// axios.defaults.baseURL = 'http://aj.kingwingaviation.com/alliance-java/'
 axios.defaults.withCredentials = true
 axios.interceptors.request.use(
   config => {
@@ -52,6 +52,11 @@ axios.interceptors.response.use(
       // const redirect = `http://aj.kingwingaviation.com/alliance/api/wechat/auth/fuwuLogin?state=${encodeURIComponent(location.href)}`
       const redirect = `http://aj.kingwingaviation.com/alliance-java/wechat/auth/fuwuLogin?state=${encodeURIComponent(location.href)}`
       window.location.href = redirect
+    } else if (error.toString().includes('400')) {
+      let url = location.host + '/#/attestation?redirect=mine'
+      setTimeout(() => {
+        window.location.href = '/#/attestation?redirect=mine'        
+      }, 800)
     }
     return Promise.reject(error)
   }
